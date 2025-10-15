e0b84448bf35d6b3dcfd3a85fd766fc4d3ef78ea
Harsh Guleria <guleriah59@gmail.com>
2025-10-15 17:55:55 +0530

    01_basics

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,9 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ************** Operations ***************
+
+let value = 3
+let negaValue = -value;
+console.log(negaValue)
