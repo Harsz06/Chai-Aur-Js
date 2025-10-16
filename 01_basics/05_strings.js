216bc043886e202c4805134f200422beef519d76
Harsh Guleria <guleriah59@gmail.com>
2025-10-16 22:47:23 +0530

    01_basics

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -12,11 +12,23 @@ const gameName = new String("hitesh-hc");
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf("t"));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("t"));
 
 const newString = gameName.substring(0, 4);
 // console.log(newString);
 
-const anotherString = gameName.slice(-8,4);
-console.log(anotherString);
+const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+const newStringOne = "   hitesh    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary";
+
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("hitesh"));
+
+console.log(gameName.split('-'))
