e8f29673241723aeb7b96d58500724ff2d2dccad
Harsh Guleria <guleriah59@gmail.com>
2025-10-24 19:27:16 +0530

    09_advance_one

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,31 @@
+const promiseOne = new Promise(function (resolve, reject) {
+  //Do an sync task
+  //Db calls, crytography, network
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve();
+  }, 1000);
+});
+
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    resolve({ username: "Chai", email: "chai@example.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
