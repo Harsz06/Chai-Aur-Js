4cdbd846b8ba7237bfd4450772b5eae2b88f027d
Harsh Guleria <guleriah59@gmail.com>
2025-10-24 23:54:39 +0530

    09_advance_one

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -78,17 +78,17 @@ async function consumePromiseFive() {
 
 consumePromiseFive();
 
-// async function getAllUsers() {
-//   try {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.log("E: ", error);
-//   }
-// }
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("E: ", error);
+  }
+}
 
-// getAllUsers();
+getAllUsers();
 
 fetch("https://api.github.com/users/hiteshchoudhary")
   .then((response) => {
