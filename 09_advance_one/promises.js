9729e9b7d39982db270a48461cc45f52c04bbf85
Harsh Guleria <guleriah59@gmail.com>
2025-10-24 22:39:46 +0530

    09_advance_one

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -55,8 +55,8 @@ promiseFour
   .finally(() => {
     console.log("The promise is either resolve or rejected");
   });
-  
-const promiseFive = new Promise((resolve,reject) => {
+
+const promiseFive = new Promise((resolve, reject) => {
   setTimeout(function () {
     let error = true;
     if (!error) {
@@ -65,6 +65,27 @@ const promiseFive = new Promise((resolve,reject) => {
       reject("ERROR: JS went wrong");
     }
   }, 1000);
-})
+});
 
-promiseFive.then()
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log("E: ", error);
+//   }
+// }
+
+// getAllUsers();
